6edf8b8ac5dead859d38249e39a6852fb2012bd8
gituppa-getonup <jerre@xs4all.nl>
2018-03-10 22:06:04 +0100

    Added the CursorLoader implementation.

---
+++ app/src/main/java/com/portablecollections/portablecollections/CollectableAdapter.java
@@ -0,0 +1,54 @@
+package com.portablecollections.portablecollections;
+
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class CollectableAdapter extends RecyclerView.Adapter<CollectableAdapter.ViewHolder> {
+
+    private Cursor mCursor;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if (mCursor.moveToPosition(position)) {
+            holder.mText.setText(mCursor.getString(
+                    mCursor.getColumnIndexOrThrow("name")
+            ));
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCursor == null ? 0 : mCursor.getCount();
+    }
+
+    public void setCollectables(Cursor cursor) {
+        mCursor = cursor;
+        notifyDataSetChanged();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mText;
+
+        ViewHolder(ViewGroup parent) {
+            super(LayoutInflater.from(parent.getContext()).inflate(
+                    android.R.layout.simple_list_item_1, parent, false));
+            mText = itemView.findViewById(android.R.id.text1);
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/portablecollections/portablecollections/MainActivity.java
+++ app/src/main/java/com/portablecollections/portablecollections/MainActivity.java
@@ -2,9 +2,15 @@ package com.portablecollections.portablecollections;
 
 import android.arch.persistence.room.Room;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -12,7 +18,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
+    private final static int LOADER_COLLECTABLES = 1;
     private static CollectableDatabase collectableDatabase;
+    private CollectableAdapter mCollectableAdapter;
 
 
     @Override
@@ -20,12 +28,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        final RecyclerView recycler1 = findViewById(R.id.recycler1);
+        recycler1.setLayoutManager(new LinearLayoutManager(recycler1.getContext()));
 
+        mCollectableAdapter = new CollectableAdapter();
+        recycler1.setAdapter(mCollectableAdapter);
+        getSupportLoaderManager().initLoader(1, null, mLoaderCallbacks);
 
-
-        }
-
+    }
 
 
 
@@ -42,4 +52,46 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("city: " + collectable.getCity());
         }
         */
+
+    private LoaderManager.LoaderCallbacks<Cursor> mLoaderCallbacks =
+            new LoaderManager.LoaderCallbacks<Cursor>() {
+                @Override
+                public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                    switch (id) {
+                        case LOADER_COLLECTABLES:
+                            return new CursorLoader(getApplicationContext(),
+                                    CollectableProvider.URI_COLLECTABLES,
+                                    new String[]{"name", "description", "country", "city"},
+                                    null,
+                                    null,
+                                    null
+                            );
+                        default:
+                            Log.e(TAG, "mLoaderCallbacks");
+                            throw new IllegalArgumentException();
+                    }
+
+                }
+
+                @Override
+                public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+                    switch (loader.getId()) {
+                        case LOADER_COLLECTABLES:
+                            mCollectableAdapter.setCollectables(cursor);
+                            break;
+                    }
+                }
+
+                @Override
+                public void onLoaderReset(Loader<Cursor> loader) {
+                    switch (loader.getId()) {
+                        case LOADER_COLLECTABLES:
+                            mCollectableAdapter.setCollectables(null);
+                            break;
+                    }
+                }
+
+
+            };
+
 }
