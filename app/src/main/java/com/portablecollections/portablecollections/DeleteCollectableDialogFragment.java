ad8bfb3fd2d9c0faf33c16f14707ad5eda137b7a
gituppa-getonup <33804658+gituppa-getonup@users.noreply.github.com>
2018-11-08 21:37:49 +0100

    Added DELETE button to details view, with a confirmation dialog.

---
+++ app/src/main/java/com/portablecollections/portablecollections/DeleteCollectableDialogFragment.java
@@ -0,0 +1,47 @@
+package com.portablecollections.portablecollections;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+public class DeleteCollectableDialogFragment extends DialogFragment {
+
+    public interface NoticeDialogListener {
+        void onDialogPositiveClick(DialogFragment dialog);
+        void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    NoticeDialogListener mListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (NoticeDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Delete this collectable?")
+                .setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onDialogPositiveClick(DeleteCollectableDialogFragment.this);
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onDialogNegativeClick(DeleteCollectableDialogFragment.this);
+                    }
+                });
+        return builder.create();
+    }
+}
